d1c667a64b31c9bc3b558e7cd53e4c20eb7c918c
jafarkhan <grihvedaweb@gmail.com>
2026-09-17 14:50:17 +0530

    Update contact page with actual Google map link

--- src/app/(store)/contact/page.tsx
+++ src/app/(store)/contact/page.tsx
@@ -68,7 +68,7 @@ export default function ContactPage() {
 
           <div className="bg-white rounded-2xl shadow-sm border border-zinc-100 overflow-hidden h-[500px]">
              <iframe 
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14766.155700813733!2d73.2081283!3d22.3168285!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395fc8aa3bc84569%3A0xc68297b5e40702c2!2sVadodara%2C%20Gujarat!5e0!3m2!1sen!2sin!4v1700000000000!5m2!1sen!2sin" 
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3691.010395550639!2d73.23101199999999!3d22.315446599999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395fcf48f812ccc9%3A0xcca1c1480fc52a68!2sAR%20FURNITURE!5e0!3m2!1sen!2sin!4v1789635051746!5m2!1sen!2sin" 
               width="100%" 
               height="100%" 
               style={{ border: 0 }} 
